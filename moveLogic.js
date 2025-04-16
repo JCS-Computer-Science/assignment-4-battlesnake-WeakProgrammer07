aa5e5f7964870690b252e8de99459f6b6a53841b
WeakProgrammer07 <liamw37@educbe.ca>
2025-04-16 00:28:00 +0000

    Improved the sensing a bit, made things functions for ease

--- moveLogic.js
+++ moveLogic.js
@@ -19,29 +19,39 @@ export default function move(gameState){
     };
     
     // We've included code to prevent your Battlesnake from moving backwards
+    
     const myHead = gameState.you.body[0];
     const myNeck = gameState.you.body[1];
 
-    moveSafety.left  = (myNeck.x < myHead.x) ? false : moveSafety.left;
+    function safeBack(){
+        moveSafety.left  = (myNeck.x < myHead.x) ? false : moveSafety.left;
     moveSafety.right  = (myNeck.x > myHead.x) ? false : moveSafety.right;
     moveSafety.down  = (myNeck.y < myHead.y) ? false : moveSafety.down;
     moveSafety.up  = (myNeck.y > myHead.y) ? false : moveSafety.up;
+    }
+    safeBack()
     
     // TODO: Step 1 - Prevent your Battlesnake from moving out of bounds
-
-    moveSafety.left = (myHead.x == 0) ? false : moveSafety.left
-    moveSafety.right = (myHead.x == (gameState.board.width) -1) ? false : moveSafety.right
-    moveSafety.down = (myHead.y == 0) ? false : moveSafety.down
-    moveSafety.up = (myHead.y == (gameState.board.height) -1) ? false : moveSafety.up
+    function bounds(){
+        moveSafety.left = (myHead.x == 0) ? false : moveSafety.left
+        moveSafety.right = (myHead.x == (gameState.board.width) -1) ? false : moveSafety.right
+        moveSafety.down = (myHead.y == 0) ? false : moveSafety.down
+        moveSafety.up = (myHead.y == (gameState.board.height) -1) ? false : moveSafety.up
+    }
+    bounds()
     
     // TODO: Step 2 - Prevent your Battlesnake from colliding with itself
     let myBody = gameState.you.body
-    for(let i = 1; i < myBody.length - 1; i++){
-        moveSafety.right = (myHead.x + 1 == myBody[i].x  && myHead.y == myBody[i].y) ? false : moveSafety.right
-        moveSafety.left = (myHead.x  -1 == myBody[i].x && myHead.y == myBody[i].y) ? false : moveSafety.left
-        moveSafety.up = (myHead.x == myBody[i].x  && myHead.y + 1== myBody[i].y ) ? false : moveSafety.up
-        moveSafety.down = (myHead.x == myBody[i].x && myHead.y -1 == myBody[i].y) ? false : moveSafety.down
+    function selfPreservation(){
+        for(let i = 1; i < myBody.length - 1; i++){
+            moveSafety.right = (myHead.x + 1 == myBody[i].x  && myHead.y == myBody[i].y) ? false : moveSafety.right
+            moveSafety.left = (myHead.x  -1 == myBody[i].x && myHead.y == myBody[i].y) ? false : moveSafety.left
+            moveSafety.up = (myHead.x == myBody[i].x  && myHead.y + 1== myBody[i].y ) ? false : moveSafety.up
+            moveSafety.down = (myHead.x == myBody[i].x && myHead.y -1 == myBody[i].y) ? false : moveSafety.down
+        }
     }
+    selfPreservation()
+    
     
     // TODO: Step 3 - Prevent your Battlesnake from colliding with other Battlesnakes
 
@@ -50,14 +60,17 @@ export default function move(gameState){
         let enemySnake = gameState.board.snakes[j];
         if (enemySnake.id === gameState.you.id) continue;
         //prevents me hitting the bodies
-        for (let i = 0; i < enemySnake.body.length; i++) {
-            const enemyBody = enemySnake.body[i];
-            moveSafety.right = (myHead.x + 1 == enemyBody.x && myHead.y == enemyBody.y) ? false : moveSafety.right
-            moveSafety.left = (myHead.x - 1 == enemyBody.x && myHead.y == enemyBody.y) ? false : moveSafety.left
-            moveSafety.up = (myHead.x == enemyBody.x && myHead.y + 1 == enemyBody.y) ? false : moveSafety.up
-            moveSafety.down = (myHead.x == enemyBody.x && myHead.y - 1 == enemyBody.y) ? false : moveSafety.down
+        function enemyDodging(){
+            for (let i = 0; i < enemySnake.body.length; i++) {
+                const enemyBody = enemySnake.body[i];
+                moveSafety.right = (myHead.x + 1 == enemyBody.x && myHead.y == enemyBody.y) ? false : moveSafety.right
+                moveSafety.left = (myHead.x - 1 == enemyBody.x && myHead.y == enemyBody.y) ? false : moveSafety.left
+                moveSafety.up = (myHead.x == enemyBody.x && myHead.y + 1 == enemyBody.y) ? false : moveSafety.up
+                moveSafety.down = (myHead.x == enemyBody.x && myHead.y - 1 == enemyBody.y) ? false : moveSafety.down
+            }
+            
         }
-
+        enemyDodging()
         let enemyHead = enemySnake.body[0];
         let myLength = gameState.you.body.length;
         let enemyLength = enemySnake.body.length;
@@ -89,6 +102,8 @@ export default function move(gameState){
                     riskyMoves.down = true
                 }
             }
+            enemyDodging()
+            selfPreservation()
         }
     }
 
@@ -111,12 +126,12 @@ export default function move(gameState){
         return totalIndex
     }
 
-    if(gameState.you.health < 65){
-        let foodIndex = findClosestFood(gameState.board.food)
-        priorityMoves.right = (gameState.board.food[foodIndex].x > myHead.x) ? true : priorityMoves.right
-        priorityMoves.left = (gameState.board.food[foodIndex].x < myHead.x) ? true : priorityMoves.left
-        priorityMoves.up = (gameState.board.food[foodIndex].y > myHead.y) ? true : priorityMoves.up
-        priorityMoves.down = (gameState.board.food[foodIndex].y < myHead.y) ? true : priorityMoves.down
+    if(gameState.you.health < 50){
+        let foodSpot = gameState.board.food[findClosestFood(gameState.board.food)]
+        priorityMoves.right = (foodSpot.x > myHead.x) ? true : priorityMoves.right
+        priorityMoves.left = (foodSpot.x < myHead.x) ? true : priorityMoves.left
+        priorityMoves.up = (foodSpot.y > myHead.y) ? true : priorityMoves.up
+        priorityMoves.down = (foodSpot.y < myHead.y) ? true : priorityMoves.down
     }
     
     // Are there any safe moves left?
@@ -130,29 +145,27 @@ export default function move(gameState){
 
     if (safeMoves.length == 0) {
         if(riskyOptions.length != 0){
-            console.log(`MOVE ${gameState.turn}: No safe moves detected! Making a risk`)
+            console.log(`MOVE ${gameState.turn}: RISKY TIME`)
         return { move: riskyOptions[Math.floor(Math.random() * riskyOptions.length)] };
         }
-        console.log(`MOVE ${gameState.turn}: No safe moves detected!`)
+        console.log(`MOVE ${gameState.turn}: Death is EVERYWHERE`)
         return { move: moveSafety[Math.floor(Math.random() * moveSafety.length)] };
     }
     
+    let futureSenseMoves = safeMoves.filter(move => futureSense(move, gameState, 5));
 
-
-    let futureSenseMoves = safeMoves.filter(move => futureSense(move, gameState, 3));
-
-    let nextPriorityMove1 = priorityMovesFinal[0];
+    let nextPriorityMove1 = priorityMovesFinal[0]; // only 2 options for food, its either up/down, or left/right
     let nextPriorityMove2 = priorityMovesFinal[1];
 
     if (futureSenseMoves.includes(nextPriorityMove1)) {
-        console.log(`Priority Lookahead MOVE (1) ${gameState.turn}: ${nextPriorityMove1}`);
+        console.log(`Priority FutureSense MOVE (1) ${gameState.turn}: ${nextPriorityMove1}`);
         return { move: nextPriorityMove1 };
     } else if (futureSenseMoves.includes(nextPriorityMove2)) {
-        console.log(`Priority Lookahead MOVE (2) ${gameState.turn}: ${nextPriorityMove2}`);
+        console.log(`Priority FutureSense MOVE (2) ${gameState.turn}: ${nextPriorityMove2}`);
         return { move: nextPriorityMove2 };
     } else if (futureSenseMoves.length > 0) {
         const bestMove = futureSenseMoves[Math.floor(Math.random() * futureSenseMoves.length)];
-        console.log(`Lookahead MOVE ${gameState.turn}: ${bestMove}`);
+        console.log(`FutureSense MOVE ${gameState.turn}: ${bestMove}`);
         return { move: bestMove };
     }
 
@@ -163,43 +176,47 @@ export default function move(gameState){
         console.log(`Priority MOVE (2) ${gameState.turn}: ${nextPriorityMove2}`)
         return { move: nextPriorityMove2 };
     }
-
+    let nextMove = safeMoves[Math.floor(Math.random() * safeMoves.length)]
     console.log(`MOVE ${gameState.turn}: ${nextMove}`)
     return { move: nextMove };
 }
-
+// checks for dead ends
 function futureSense(move, gameState, depth) {
     const newGameState = JSON.parse(JSON.stringify(gameState));
 
     const newHead = { ...newGameState.you.body[0] };
-    if (move === "up") newHead.y += 1;
-    if (move === "down") newHead.y -= 1;
-    if (move === "left") newHead.x -= 1;
-    if (move === "right") newHead.x += 1;
+    if (move == "up") newHead.y += 1;
+    if (move == "down") newHead.y -= 1;
+    if (move == "left") newHead.x -= 1;
+    if (move == "right") newHead.x += 1;
 
     newGameState.you.body.unshift(newHead);
     newGameState.you.health -= 1;
     if (
         newHead.x < 0 || newHead.x >= newGameState.board.width ||
         newHead.y < 0 || newHead.y >= newGameState.board.height
-    ) return false;
+    ){
+        return false;
+    } 
 
     for (let i = 1; i < newGameState.you.body.length; i++) {
-        if (newHead.x === newGameState.you.body[i].x &&
-            newHead.y === newGameState.you.body[i].y) {
+        if (newHead.x == newGameState.you.body[i].x &&
+            newHead.y == newGameState.you.body[i].y) {
             return false;
         }
     }
 
-    for (let snake of newGameState.board.snakes) {
-        if (snake.id === newGameState.you.id) continue;
-        for (let body of snake.body) {
-            if (newHead.x === body.x && newHead.y === body.y) {
+    for (let snake of newGameState.board.snakes) { // so much easier than a normal for loop
+        if (snake.id == newGameState.you.id) continue; // once again ignore myself
+        for (let body of snake.body) { // for each snake (doesn't account for them moving yet)
+            if (newHead.x == body.x && newHead.y == body.y) {
                 return false;
             }
         }
     }
-    if (depth <= 0) return true;
+    if (depth <= 0) {
+        return true;
+    }
     const nextMoves = ["up", "down", "left", "right"];
     for (let nextMove of nextMoves) {
         if (futureSense(nextMove, newGameState, depth - 1)) {
